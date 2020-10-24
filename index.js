99a6006e12d60d0d76f004a3d95d3ef7eccd5f6e
luckmer <woszczo04@gmail.com>
2020-10-24 12:28:56 +0200

    js update

--- index.js
+++ index.js
@@ -5,49 +5,43 @@ document.addEventListener("DOMContentLoaded", () =>{
     let gravity = 3;
     let ChTimer;
     let ChLeft = 10;
-    function FallDown()
-    {
-        ChTimer = setInterval(function ()
-        {
+
+    function FallDown(){
+        ChTimer = setInterval(function (){
             characterLeft += gravity;
             character.style.top = characterLeft + "px"
             character.style.left = ChLeft + "px"
-            if (characterLeft > 355)
-            {
+            if (characterLeft > 355){
                 characterLeft -= gravity;
             }
-            
         }, 20)
     }
 
-    function generateWall()
-    {
+    function generateWall(){
+        
         let left = 830
         let random = ((Math.random() * 120) + 250)
         const wall = DownPart(random);
         const UpWall = upperPart(random, Game);
         Game.appendChild(wall)
 
-        function moveBlock()
-        {
+        function moveBlock(){
             left -= 5;
             wall.style.left = left + "px"
             UpWall.style.left = left + "px"
-            if (left < - 100)
-            {
+            if (left < -100){
                 clearInterval(timerId)
                 wall.classList.remove("wall")
                 UpWall.classList.remove("UpWall")
             }
-            Collision(left, ChLeft, characterLeft, random, setEndGame, timerId, ChTimer, timeControl);
+            Collision(left, ChLeft, characterLeft, random, setEndGame, timerId, ChTimer);
         }
         let timerId = setInterval(moveBlock, 20)
-        let timeControl =  setTimeout(generateWall, 3000)
-        
+        setTimeout(generateWall, 3000)
     }
 
-    function upperPart(random, Game)
-    {
+
+    function upperPart(random, Game){
         const UpWall = document.createElement("div");
         UpWall.classList.add("UpWall");
         UpWall.style.top = random - 350 + "px";
@@ -55,46 +49,39 @@ document.addEventListener("DOMContentLoaded", () =>{
         return UpWall;
     }
 
-    function DownPart(random)
-    {
+    function DownPart(random){
         const wall = document.createElement("div");
         wall.classList.add("wall");
         wall.style.top = random + "px";
         return wall;
     }
     
-    function Collision(left, ChLeft, characterLeft, random, setEndGame, timerId, ChTimer, timeControl)
-    {
+    function Collision(left, ChLeft, characterLeft, random, setEndGame, timerId, ChTimer){
         if (left > -10 && left < 10 &&
             ChLeft === 10 &&
             (characterLeft < random - 180) ||
-            characterLeft === 0)
-        {
-            setEndGame(timerId, ChTimer, timeControl);
+            characterLeft === 0){
+            setEndGame(timerId, ChTimer);
         }
     
         if (left > -10 &&
             left < 10 &&
             ChLeft === 10 &&
-            (characterLeft > random - 20))
-        {
-            setEndGame(timerId, ChTimer, timeControl);
+            (characterLeft > random - 20)){
+            setEndGame(timerId, ChTimer);
         }
+
     }
 
-    function setEndGame(timerId, ChTimer, timeControl)
-    {
+    function setEndGame(timerId, ChTimer){
         clearInterval(timerId);
-        clearInterval(ChTimer);
-        clearTimeout(timeControl);
+        clearInterval(ChTimer);       
     }
-    
+
     const jump = () => characterLeft < 910 ? characterLeft -= 50 : characterLeft -= 2;
     
-    function Joystick(e)
-    {
-        if (e.keyCode === 32)
-        {
+    function Joystick(e){
+        if (e.keyCode === 32){
             jump()
         }
     }
